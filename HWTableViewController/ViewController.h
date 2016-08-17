f53952e491ec5e384dc9f91d88ebd3b49658c0b2
Ильдар Залялов <Zalyalov.Ild@gmail.com>
2016-08-17 09:09:20 +0300

    Initial Commit

---
+++ HWTableViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWTableViewController
+//
+//  Created by Ildar Zalyalov on 17.08.16.
+//  Copyright © 2016 com.itisiOSlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
